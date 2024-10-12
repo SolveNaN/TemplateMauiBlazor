5298d606f8afb4fd0f2cebbf8bf9ed0164eba7bb
SolveNaN <dagoberto6993@gmail.com>
2024-10-12 03:02:45 -0500

    first commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TemplateMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
